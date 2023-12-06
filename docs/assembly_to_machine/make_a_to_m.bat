7451464dbad1f9675de2099fe0f8f11302ead929
Engineer-Ayesha-Shafique <shafiqr504@gmail.com>
2023-12-06 20:51:24 +0500

    updated bat files

--- docs/assembly_to_machine/make_a_to_m.bat
+++ docs/assembly_to_machine/make_a_to_m.bat
@@ -1,9 +1,9 @@
 rm -r build
 mkdir build
-/home/dell/Downloads/riscv64-unknown-elf-toolchain-10.2.0-2020.12.8-x86_64-linux-ubuntu14/bin/riscv64-unknown-elf-as -c -o build/asm_code.o ../../sim/asm_code.s -march=rv32i -mabi=ilp32
-/home/dell/Downloads/riscv64-unknown-elf-toolchain-10.2.0-2020.12.8-x86_64-linux-ubuntu14/bin/riscv64-unknown-elf-gcc -o build/asm_code.elf build/asm_code.o -T linker.ld -nostdlib -march=rv32i -mabi=ilp32
-/home/dell/Downloads/riscv64-unknown-elf-toolchain-10.2.0-2020.12.8-x86_64-linux-ubuntu14/bin/riscv64-unknown-elf-objcopy -O binary --only-section=.data* --only-section=.text* build/asm_code.elf build/asm_code.bin
+riscv64-unknown-elf-as -c -o build/asm_code.o ../../sim/asm_code.s -march=rv32i -mabi=ilp32
+riscv64-unknown-elf-gcc -o build/asm_code.elf build/asm_code.o -T linker.ld -nostdlib -march=rv32i -mabi=ilp32
+riscv64-unknown-elf-objcopy -O binary --only-section=.data* --only-section=.text* build/asm_code.elf build/asm_code.bin
 python3 maketxt.py build/asm_code.bin > build/asm_code.txt
-/home/dell/Downloads/riscv64-unknown-elf-toolchain-10.2.0-2020.12.8-x86_64-linux-ubuntu14/bin/riscv64-unknown-elf-objdump -S -s build/asm_code.elf > build/asm_code.dump
+riscv64-unknown-elf-objdump -S -s build/asm_code.elf > build/asm_code.dump
 
 cp "build/asm_code.txt" "../../inst.mem"
